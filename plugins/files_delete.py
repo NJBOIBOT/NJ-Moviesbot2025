243e411f092cdd7a1f691ec232374deb4cd7831c
NJBOIBOT <144768572+NJBOIBOT@users.noreply.github.com>
2025-08-06 16:42:29 +0530

    Update files_delete.py

--- plugins/files_delete.py
+++ plugins/files_delete.py
@@ -21,8 +21,8 @@ async def deletemultiplemedia(bot, message):
     else:
         return
 
-    file_id, file_ref = unpack_new_file_id(media.file_id)
-
+    file_id, file_ref, *_ = unpack_new_file_id(media.file_id)
+    
     result = col.delete_one({
         'file_id': file_id,
     })
@@ -64,3 +64,4 @@ async def deletemultiplemedia(bot, message):
                 logger.info('File is successfully deleted from database.')
             else:
                 logger.info('File not found in database.')
+
