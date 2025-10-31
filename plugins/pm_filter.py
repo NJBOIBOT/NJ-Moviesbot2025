d1573351bfd240528d279cb8d9ca2d85cb7f4481
NJBOIBOT <144768572+NJBOIBOT@users.noreply.github.com>
2025-10-31 13:38:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -33,10 +33,12 @@ SPELL_CHECK = {}
 
 @Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
+    if message.from_user is None:
+        return
     if message.chat.id != SUPPORT_CHAT_ID:
         import re
-        link_pattern = r"(https?://|t\.me/|www\.)"
-        if re.search(link_pattern, message.text):
+        link_pattern = r"(https?://|http://|www\.|t\.me/|[a-zA-Z0-9_-]+\.[a-z]{2,})"
+        if re.search(link_pattern, message.text, re.IGNORECASE):
             return  # 🚫 Skip messages with links
 
         settings = await get_settings(message.chat.id)
@@ -3320,4 +3322,5 @@ async def global_filters(client, message, text=False):
                 break
     else:
         return False
+
 
